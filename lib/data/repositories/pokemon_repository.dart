e325e60590f87c70e1f79d728f6b1b38f6d28879
Verrelio C. Rizky <verrelio@prospace-ap.io>
2026-02-12 15:08:09 +0700

    create pokemon repository

---
+++ lib/data/repositories/pokemon_repository.dart
@@ -0,0 +1,42 @@
+import 'package:pokedex_flutter/core/network/api_client.dart';
+import 'package:pokedex_flutter/core/network/api_constant.dart';
+import 'package:pokedex_flutter/data/models/pokemon_detail_model.dart';
+import 'package:pokedex_flutter/data/models/pokemon_list_model.dart';
+
+class PokemonRepository {
+  final ApiClient apiClient;
+
+  PokemonRepository({
+    required this.apiClient
+  });
+
+  //Get Pokemon List
+  Future<PokemonList> getPokemonList(int limit, int offset) async {
+    try {
+      final response = await apiClient.get(
+        ApiConstant.pokemonList,
+        queryParams: {
+          'limit': limit,
+          'offset': offset
+        }
+      );
+
+      return PokemonList.fromJson(response.data);
+    } catch {
+      throw Exception('Failed to get pokemons data: $e');
+    }
+  }
+
+  //Get Pokemon Detail
+  Future<PokemonDetail> getPokemonDetail(String name) async {
+    try {
+      final response  = await apiClient.get(
+        ApiConstant.pokemonDetail.replaceFirst('{name}', name)
+      );
+
+      return PokemonDetail.fromJson(response.data);
+    } catch {
+      throw Exception('Failed to get pokemon data: $e');
+    }
+  }
+}
